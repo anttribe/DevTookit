c599c7e702a69d84c3f4a78e7f276ab66754695d
anttribe <anshenglimin@gmail.com>
2014-06-14 18:07:19 +0800

    iframe嵌入问题优化

--- background.js
+++ background.js
@@ -3,22 +3,24 @@
  */
 var dev_toolkit_uuid = null;
 
-function dispayIndex(tab) {
-    if(dev_toolkit_uuid){
-        chrome.tabs.update(dev_toolkit_uuid, {
-            active: true
-        });
-    } else{
-        chrome.tabs.create({
-            url: chrome.extension.getURL("../../popup.html")
-        }, function(tab){
-            dev_toolkit_uuid = tab.id;
-        });
-    }
-};
+var displayIndex = function(){
+    return function() {
+        if(dev_toolkit_uuid){
+            chrome.tabs.update(dev_toolkit_uuid, {
+                active: true
+            });
+        } else{
+            chrome.tabs.create({
+                url: chrome.extension.getURL("popup.html")
+            }, function(tab){
+                dev_toolkit_uuid = tab.id;
+            });
+        }
+    };
+}();
 
 chrome.browserAction.onClicked.addListener(function(tab){
-    dispayIndex(tab);
+    displayIndex(tab);
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId){
--- resources/js/navigator.js
+++ resources/js/navigator.js
@@ -1,3 +1,52 @@
 /**
  * Created by zhaoyong on 2014-6-14.
+ * 用于页面上导航的处理
  */
+(function($){
+    $(document).ready(function(){
+        $('.tabs').children('li').click(function(){
+            // 所有兄弟li元素去除active样式
+            $(this).siblings('li').removeClass('active');
+            $('.tab-content').children().removeClass("show").addClass("hidden");
+
+            // 取得a标签获得href属性值，得到需要显示的区域块id
+            var href = $(this).children('a').attr('href');
+            if(href){
+                $('.tab-content').children('' + href + '').each(function(){
+                    if(!$(this).attr('initialized')){
+                        var src = $(this).attr('src');
+                        if(src){
+                            $('<iframe>', {
+                                src: src,
+                                frameborder: 'no',
+                                height: '800',
+                                width: '100%',
+                                scrolling: 'no'
+                            }).bind('resize', function(){
+                                if(this.contentWindow){
+                                    var contentHeight = Math.max(this.contentWindow.document.documentElement.scrollHeight, this.contentWindow.document.body.scrollHeight);
+                                    if(contentHeight){
+                                        alert(window);
+                                        window.height = $(this).height = contentHeight;
+                                    }
+                                }
+                            }).appendTo($(this));
+                        }
+
+                        $(this).attr({'initialized':'initialized'});
+                    }
+                    //当前页签显示
+                    $(this).removeClass('hidden').addClass('show');
+                });
+
+                //当前节点添加active样式
+                $(this).addClass('active');
+            }
+        });
+
+        // 默认展示第一个页签
+        if($('li.active', $('.tabs')).length == 0){
+            $('li:first', $('.tabs')).click();
+        }
+    });
+})(jQuery);
