c97a22defd49afa1c15938e3b6b9b8060b9c42a6
anttribe <anshenglimin@gmail.com>
2014-07-22 20:00:52 +0800

    增加HTTP状态码参考页面

--- resources/js/navigator.js
+++ resources/js/navigator.js
@@ -2,38 +2,44 @@
  * Created by zhaoyong on 2014-6-14.
  * 用于页面上导航的处理
  */
-(function($){
-    $(document).ready(function(){
-        $('.tabs').children('li').click(function(){
+(function ($) {
+    $(document).ready(function () {
+        $('.tabs').children('li').click(function () {
             // 所有兄弟li元素去除active样式
             $(this).siblings('li').removeClass('active');
             $('.tab-content').children().removeClass("show").addClass("hidden");
 
             // 取得a标签获得href属性值，得到需要显示的区域块id
             var href = $(this).children('a').attr('href');
-            if(href){
-                $('.tab-content').children('' + href + '').each(function(){
-                    if(!$(this).attr('initialized')){
+            if (href) {
+                $('.tab-content').children('' + href + '').each(function () {
+                    if (!$(this).attr('initialized')) {
                         var src = $(this).attr('src');
-                        if(src){
+                        if (src) {
+                            function iframeSelfAdaption(iframe) {
+                                if (iframe) {
+                                    if (iframe.contentWindow) {
+                                        var contentHeight = Math.max(iframe.contentWindow.document.documentElement.scrollHeight, iframe.contentWindow.document.body.scrollHeight);
+                                        var windowHeight = Math.max(window.document.documentElement.scrollHeight, window.document.body.scrollHeight);
+                                        var maxHeight = Math.max(contentHeight, windowHeight);
+                                        $(iframe).height(maxHeight);
+                                        $(window).height(maxHeight);
+                                    }
+                                    var win = iframe.window;
+                                    console.log(win);
+                                }
+                            };
                             $('<iframe>', {
                                 src: src,
                                 frameborder: 'no',
                                 width: '100%',
                                 scrolling: 'no'
-                            }).bind('load', function(){
-                                if(this.contentWindow){
-                                    var contentHeight = Math.max(this.contentWindow.document.documentElement.scrollHeight, this.contentWindow.document.body.scrollHeight);
-                                    var windowHeight = Math.max(window.document.documentElement.scrollHeight, window.document.body.scrollHeight);
-                                    var maxHeight = Math.max(contentHeight, windowHeight);
-
-                                    $(this).attr('height', maxHeight);
-                                    $(window).attr('height', maxHeight);
-                                }
+                            }).bind('load', function () {
+                                iframeSelfAdaption(this);
                             }).appendTo($(this));
                         }
 
-                        $(this).attr({'initialized':'initialized'});
+                        $(this).attr({'initialized': 'initialized'});
                     }
                     //当前页签显示
                     $(this).removeClass('hidden').addClass('show');
@@ -45,7 +51,7 @@
         });
 
         // 默认展示第一个页签
-        if($('li.active', $('.tabs')).length == 0){
+        if ($('li.active', $('.tabs')).length == 0) {
             $('li:first', $('.tabs')).click();
         }
     });
